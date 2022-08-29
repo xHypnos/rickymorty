4a3b3a4f4fab0926d19604e3f8dbaab24c430a9d
Wilson <hypnoz@live.com>
2022-08-28 22:55:31 -0500

    first commi

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import imageRick from "./img/rick-morty.png";
+import { useState } from 'react';
+import Characters from './components/Characters';
+
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async() =>{
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters?(
+          <Characters characters={characters} setCharacters={setCharacters}></Characters>
+        ):(
+          <>
+          <img src={imageRick} alt="rickmorty" className="img-home" />
+          <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
